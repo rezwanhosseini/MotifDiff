c75f9b3cc955bac0f4c6006aa37c2b9e8c0a242e
rezwanhosseini <71412645+rezwanhosseini@users.noreply.github.com>
2024-02-15 18:00:07 -0500

    dinuc mat added

--- util.py
+++ util.py
@@ -10,8 +10,8 @@ from scipy.optimize import curve_fit
 from scipy.interpolate import PchipInterpolator
 import regex as re
 import itertools as itt
-import diNucMat as dnma
-import diNucMotDist as dnsd
+from collections import namedtuple
+
 
 torch.set_printoptions(precision=8)
 np.set_printoptions(precision=8)
@@ -72,6 +72,117 @@ def scoreDist(pwm, nucleotide_prob=None, gran=None, size=1000):
     support = support_min + (ii) * gran
     return support, distribution[ii]
 
+class diNucMat:
+    def __init__(self, values, colnames) -> None:
+        #- check if colnames is a 1-D array with 16 elements, each a dinucleotide in the correct order
+        assert colnames == ["".join(i) for i in itt.product(["A","C","G","T"], repeat=2)]
+        self.values   = values
+        self.colnames = colnames      
+    @property
+    def values(self):
+        return self._values    
+    @values.setter
+    def values(self, values):
+        #- check if probs is a 2-D array with 16 columns
+        assert len(values.shape) == 2
+        assert values.shape[1] == 16
+        assert values.shape[0] > 0
+        self._values = values
+        #- make the column matrices, rely on correct column order
+        #  therefore need the assert in init...
+        self._colMats = np.reshape(self.values, newshape=(values.shape[0], 4, 4))
+    @property
+    def colMats(self):
+        return self._colMats        
+    @property
+    def colnames(self):
+        return self._colnames    
+    @colnames.setter
+    def colnames(self, colnames):
+        self._colnames = colnames
+    
+
+class diNucProbMat(diNucMat):
+    @diNucMat.values.setter
+    def values(self, values):
+        assert len(values.shape) == 2
+        assert values.shape[1] == 16
+        assert values.shape[0] > 0
+        assert np.all(values >= 0)
+        assert np.all(values <= 1)
+        assert np.all(np.isclose(np.sum(values, axis=1), 1))
+        self._values = values
+        self._colMats = np.reshape(self.values, newshape=(self.values.shape[0], 4, 4))
+        self._trnMats = self._colMats / np.sum(self._colMats, axis=2, keepdims=True) 
+    @property
+    def trnMats(self) -> np.array:
+        return self._trnMats
+        
+
+
+def diNucMotDist(pssm, prob, gran=None, size=1000):
+    #- assert pssm  is a ov class diNucMat and 
+    #  prob is of class diNucProbMat
+    assert isinstance(pssm, diNucMat)    
+    assert isinstance(prob, diNucProbMat)
+
+    if gran is None:
+        if size is None:
+            raise ValueError("provide either gran or size. Both missing.")
+        gran = (np.max(pssm) - np.min(pssm))/(size - 1)  
+        
+    # utility function for converting a score to an index (of the distribution)
+    def vals2inds(vals, mnscore, gran):
+        return np.rint(((vals - mnscore)/gran)).astype(int)
+    
+    # discretization and score range
+    mnscore = np.floor(np.sum(np.min(pssm.values, axis=1))) #- lower bound
+    mxscore = np.ceil(np.sum(np.max(pssm.values, axis=1))) #- upper bound
+    if mxscore<0: mxscore=0
+    nscores = int(np.rint((mxscore - mnscore) / gran) + 1) #- shouldn't really need to round
+    
+    # make the score distribution
+    #- INITIALIZATION
+    SD = np.zeros((4, nscores))
+    #- FIRST POSITION
+    for i in range(4):
+        #- update SD at the right indices
+        for j in range(4):
+            SD[i, vals2inds(pssm.colMats[0, j, i], mnscore, gran)] += prob.colMats[0, j, i]
+
+    #- need a copy of SD for updating
+    SD_tmp = SD.copy()
+    #- ITERATE through rest of the motif
+    for pos in range(1, pssm.values.shape[0]):
+        #- nuc is ending nucleotide (second of the two)
+        for nuc in range(4):
+            tvec   = np.zeros(nscores)
+            scores = pssm.colMats[pos, :, nuc]
+            shifts = np.rint(scores / gran).astype(int)
+            #- i is the starting nucleotide
+            for i in range(4):
+                tvec += np.roll(SD_tmp[i, :], shifts[i]) * prob.trnMats[pos, i, nuc] 
+            #- overwriteing SD here, so thats why we need to use SD_tmp above
+            SD[nuc, :] = tvec
+        #- need to update SD_tmp for the next iteration
+        SD_tmp = SD.copy()
+
+    # RETURN final motif distribution
+
+    x = np.arange(mnscore, mxscore + gran, gran)
+    y = np.sum(SD, axis=0)
+    ii = np.where(y > 0)[0]
+    support = mnscore + (ii) * gran
+    
+    x = support
+    y = y[ii]
+    
+    #- results as a named tuple
+    Dist = namedtuple('Dist', ['x', 'y'])
+    return Dist(x, y)
+
+
+
 def mono2di(ppm): 
     num_rows = ppm.shape[0]
     num_cols = ppm.shape[1]** 2
@@ -84,7 +195,7 @@ def mono2di(ppm):
 def scoreDistDinuc(pwm, gran=None, size=1000):
     tmp  = pwm 
     cn   = ["".join(i) for i in itt.product(["A","C","G","T"], repeat=2)]
-    pssm = dnma.diNucMat(tmp, cn)
+    pssm = diNucMat(tmp, cn)
 
     if gran is None:
         if size is None:
@@ -94,15 +205,15 @@ def scoreDistDinuc(pwm, gran=None, size=1000):
     tmp  = np.exp(tmp)
     tmp  = tmp / np.sum(tmp, axis=1, keepdims=True)   
 
-    prob = dnma.diNucProbMat(tmp, cn)
+    prob = diNucProbMat(tmp, cn)
     # calculating the score dist
-    sd_mot = dnsd.score_dist(pssm, prob, gran=0.01)
+    sd_mot = diNucMotDist(pssm, prob, gran=0.01)
 
     #- now let's make a dinucleotide model with iid columns
     avg_dinuc_freqs = prob.values.mean(axis=0)
     iid_prob_values = np.repeat(avg_dinuc_freqs,pwm.shape[0]).reshape((pwm.shape[0],16), order = 'F')
-    prob_bg1        = dnma.diNucProbMat(iid_prob_values, cn)
-    sd_bg1          = dnsd.score_dist(pssm, prob_bg1, gran=0.01)
+    prob_bg1        = diNucProbMat(iid_prob_values, cn)
+    sd_bg1          = diNucMotDist(pssm, prob_bg1, gran=0.01)
     
     return(sd_mot.x, sd_bg1.y, sd_mot.y)
 
