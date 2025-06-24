485a8fec2c060abec65b9fb2e2dd7d0977c07ac6
rezwanhosseini <71412645+rezwanhosseini@users.noreply.github.com>
2025-06-24 11:46:09 -0400

    strand option added

--- MotifDiff.py
+++ MotifDiff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from torch.nn import functional as F
 import pandas as pd
-from .util import vcfData, MEME_probNorm, MEME_FABIAN, MCspline_fitting, mc_spline #
+from util import vcfData, MEME_probNorm, MEME_FABIAN, MCspline_fitting, mc_spline #
 import torch
 import numpy as np
 from enum import Enum
@@ -49,11 +49,16 @@ class score_type(str, Enum):
 class nucleotide_type(str, Enum):
     mono = "mono"
     di = "di"
+
+class strand_type(str, Enum):
+    fwd = "+"
+    rev = "-"
     
 @app.command()
 def variantdiff(genome: str = typer.Option(..., help="fasta file for the genome"),
                 motif_file: str = typer.Option(..., "--motif", help="meme file for the motifs"),
                 vcf: str = typer.Option(..., help="vcf file"), 
+                strand: strand_type = typer.Option(None, "--strand", help="whether you want to look on +/- strand of the sequence (default is +)"),
                 diff_score: score_type = typer.Option(score_type.NONE, "--method", help="how to calculate the diff score (FABIAN/probNorm/NONE)"),
                 max_scale: bool = typer.Option(False, "--MaxScale", help="Apply max transformation or not"),
                 nucleotide: nucleotide_type = typer.Option(nucleotide_type.mono, "--nuc", help="length of the nucleotides in the motifs (mono/di)"),
@@ -67,17 +72,23 @@ def variantdiff(genome: str = typer.Option(..., help="fasta file for the genome"
                 ):
     
     s = time.time()
+    if strand is None:
+        strand_specific = False
+        strand = '+'
+    else:
+        strand_specific = True
+        strand = strand
 
     kernel = kernel.value
     if kernel == "PWM":
         if diff_score == "FABIAN":
             motif = MEME_FABIAN()
             print(f"Reading the motifs from {motif_file}")
-            kernels, kernel_mask, kernel_norms = motif.parse(motif_file, nuc=nucleotide)
+            kernels, kernel_mask, kernel_norms = motif.parse(motif_file, nuc=nucleotide, strand_specific=strand_specific)
         else:
             if normalization_file is None:
                 motif = MEME_probNorm()
-                kernels, kernel_mask = motif.parse(motif_file, nuc=nucleotide, transform=max_scale)
+                kernels, kernel_mask = motif.parse(motif_file, nuc=nucleotide, transform=max_scale, strand_specific=strand_specific)
                 if diff_score=="probNorm":
                     print("normalization params not given. normalization process running...")
                     spline_list = MCspline_fitting(kernels, nuc=nucleotide)
@@ -99,23 +110,27 @@ def variantdiff(genome: str = typer.Option(..., help="fasta file for the genome"
             windowsize=window
 
 
-    
-    segments = vcfData(vcf, batch, genome, windowsize, dinucleotide=(nucleotide=="di"))
+    print(f"looking on {strand} strand for motif matchs on windowsize={windowsize}")
+    segments = vcfData(vcf, batch, genome, windowsize, dinucleotide=(nucleotide=="di"), strand=strand)
     
     outRef = np.empty((segments.n, motif.nmotifs), dtype=np.float32)
     outAlt = np.empty((segments.n, motif.nmotifs), dtype=np.float32)
+    refMaxPos = np.empty((segments.n, motif.nmotifs), dtype=np.float32)
+    altMaxPos = np.empty((segments.n, motif.nmotifs), dtype=np.float32)
     alpha = 0.1
     print(f"Batch size: {batch}")
-    print("Calculating convolutions")
+    print("Calculating convolutions...")
     for i in range(len(segments)):
         print(f"Batch {i+1}:")
         i1, i2 = i*batch, (i+1)*batch
         if i2 >= segments.n: i2 = segments.n
         matref, maskref, matalt, maskalt = segments[i]
+        #print('before convolution', matref.shape, matref[0,0:5,:]-matalt[0,0:5,:])
 
         ref = F.conv1d(matref, kernels)
         alt = F.conv1d(matalt, kernels)
-        
+        #print('after convolution', ref.shape, ref[0,0:5,:]-alt[0,0:5,:])
+        #print("ref shape after conv:", ref.shape)
         if window==0:
             motif_mask = F.conv1d(maskref, kernel_mask)
             ref[motif_mask == 0] = -torch.inf
@@ -131,6 +146,7 @@ def variantdiff(genome: str = typer.Option(..., help="fasta file for the genome"
             alt = np.squeeze(alt).numpy()
             
         if diff_score == "probNorm":
+            #print('before anything', ref.shape, ref[0,0:5,:]-alt[0,0:5,:])
             if mode == "average":
                 ref = ref.view(ref.shape[0], ref.shape[1]//2, ref.shape[2]*2)
                 alt = alt.view(alt.shape[0], alt.shape[1]//2, alt.shape[2]*2)
@@ -143,14 +159,25 @@ def variantdiff(genome: str = typer.Option(..., help="fasta file for the genome"
             if mode == "max":
                 ref = ref.view(ref.shape[0], ref.shape[1]//2, ref.shape[2]*2)
                 alt = alt.view(alt.shape[0], alt.shape[1]//2, alt.shape[2]*2)
+                #print(ref[0,0,:])
                 ref = np.nan_to_num(ref, nan=0)
                 alt = np.nan_to_num(alt, nan=0)
+                #print('before max pool', ref.shape)
+                _,ref_max_pos = torch.max(torch.tensor(ref[:,:,:windowsize]), dim=2)
+                _,alt_max_pos = torch.max(torch.tensor(alt[:,:,:windowsize]), dim=2)
+                #print(ref_max_pos, ref_max_pos.shape)
+                #print(alt_max_pos, alt_max_pos.shape)
                 ref = F.max_pool1d(torch.tensor(ref), ref.shape[2])
                 alt = F.max_pool1d(torch.tensor(alt), alt.shape[2])
+                #print('after max pool', ref.shape, ref[0,0:5,:]-alt[0,0:5,:])
+
                 ref = mc_spline(ref, spline_list)
                 alt = mc_spline(alt, spline_list)
+                #print('after spline', ref.shape)
+
             ref = np.squeeze(ref).numpy()
             alt = np.squeeze(alt).numpy()
+
             
         if diff_score == "FABIAN":                 
             ref = F.max_pool1d(ref, ref.shape[2]).numpy()
@@ -160,6 +187,8 @@ def variantdiff(genome: str = typer.Option(..., help="fasta file for the genome"
         
         outRef[i1:i2, :motif.nmotifs] = ref 
         outAlt[i1:i2, :motif.nmotifs] = alt
+        refMaxPos[i1:i2, :motif.nmotifs] = ref_max_pos
+        altMaxPos[i1:i2, :motif.nmotifs] = alt_max_pos
 
     print("calculating diff...")
     if diff_score == "FABIAN":
@@ -183,6 +212,10 @@ def variantdiff(genome: str = typer.Option(..., help="fasta file for the genome"
     write_output_diff(out_file+f"_{nucleotide}_{diff_score}_{mode}.alt", outAlt, motif_names, segments.names())
     write_output_diff(out_file+f"_{nucleotide}_{diff_score}_{mode}.ref", outRef, motif_names, segments.names())
     write_output_diff(out_file+f"_{nucleotide}_{diff_score}_{mode}.diff", outDiff, motif_names, segments.names())
+
+    write_output_diff(out_file+f"_{nucleotide}_{diff_score}.refMaxPos", refMaxPos, motif_names, segments.names())
+    write_output_diff(out_file+f"_{nucleotide}_{diff_score}.altMaxPos", altMaxPos, motif_names, segments.names())
+
     
     e = time.time()
     print(f"real runtime for {out_file}_{nucleotide}_{diff_score}_{mode} = ", e-s)
